9a96b4610c59351f0c27703d3ef2a55cdc7dabf3
XIE Yuheng <xyheme@gmail.com>
2017-10-02 06:12:28 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='jojo',
+      version='0.0.1',
+      description="jojo's programming adventure on python",
+      url='http://github.com/xieyuheng/jojo-on-python',
+      author='xieyuheng',
+      author_email='xyheme@gmail.com',
+      packages=['jojo'],
+)
