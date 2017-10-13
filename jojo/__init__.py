a55af675e92d82dfae999801fd01fbda5fde5467
XIE Yuheng <xyheme@gmail.com>
2017-10-13 09:37:18 +0800

    jojo as a python package

---
+++ jojo/__init__.py
@@ -0,0 +1,16 @@
+from jojo.jojo import (
+    VM, RP,
+    GET, SET,
+    JOJO, MSG, CLO,
+    APPLY, IFTE,
+    NEW,
+    CALL,
+
+    scan_string_vect,
+    parse_sexp_vect,
+    sexp_print,
+
+    compile_module,
+
+    load, run, repl,
+)
--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('README') as f:
     readme = f.read()
 
 setup(
     name='jojo',
-    version='0.0.9',
+    version='0.0.10',
     description="jojo's programming adventure in python",
     url='http://github.com/xieyuheng/jojo-on-python',
 
     author='xieyuheng',
     author_email='xyheme@gmail.com',
 
-    py_modules=['jojo'],
-    scripts=['jojo'],
-    package_data={'': ['core.jo']},
-    packages=find_packages(exclude=('tests', 'docs')),
+
+    packages=['jojo'],
+    package_data={'jojo': ['*.jo']},
+    scripts=['jojo/jojo'],
 
     long_description=readme,
     license="I dedicate all my works here to all human beings.",
