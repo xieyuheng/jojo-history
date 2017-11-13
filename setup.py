5e10743687ee79ca1c7146d763341625c97c1187
xieyuheng <xyheme@gmail.com>
2017-11-13 16:59:30 +0800

    up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README') as f:
 
 setup(
     name='jojo',
-    version='0.0.15',
+    version='0.0.18',
     description="jojo's programming adventure in python",
     url='http://github.com/xieyuheng/jojo-on-python',
 
